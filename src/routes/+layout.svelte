6c16892256a9e95ff2ee037914ecb3df03ee0e0d
Joe Attardi <jattardi@gmail.com>
2024-07-29 21:54:20 -0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import '../app.css';
+</script>
+
+<svelte:head>
+  <title>EchoBuddy</title>
+</svelte:head>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import ChatText from 'phosphor-svelte/lib/ChatText';
+</script>
+
+<main class="p-4">
+  <h1 class="text-2xl flex items-center justify-center gap-2">
+    <ChatText />
+    EchoBuddy
+  </h1>
+  <div class="w-2/3 m-auto text-center">
+    <!-- Add the form and text field here -->
+  </div>
+</main>
