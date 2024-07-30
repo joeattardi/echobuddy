68e4babd93a91fb1cf311243ed3caa1c2dcfffb8
Joe Attardi <jattardi@gmail.com>
2024-07-29 22:04:49 -0400

    Implement Project 1, Milestone 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script>
   import ChatText from 'phosphor-svelte/lib/ChatText';
+
+  let response = '';
+
+  function handleQuery(event) {
+    event.preventDefault();
+    response = 'Thanks for your question, we will have an answer for you soon!';
+  }
 </script>
 
 <main class="p-4">
@@ -8,6 +15,11 @@
     EchoBuddy
   </h1>
   <div class="w-2/3 m-auto text-center">
-    <!-- Add the form and text field here -->
+    <form on:submit={handleQuery}>
+      <input type="text" name="query" class="border border-slate-800 w-full rounded p-2">
+    </form>
+    <div class="mt-4">
+      {response}
+    </div>
   </div>
 </main>
