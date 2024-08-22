61cba62b10a35cd6b75d4428c01a8be613fce012
Joe Attardi <jattardi@gmail.com>
2024-08-21 21:55:13 -0400

    Implement Project 2, Milestone 3

--- src/lib/functions/getWeather.js
+++ src/lib/functions/getWeather.js
@@ -4,14 +4,32 @@ export const schema = {
   type: 'function',
   function: {
     name: 'getWeather',
-    description: 'Gets the weather conditions for a given location.'
+    description: 'Gets the weather conditions for a given location.',
+    parameters: {
+      type: 'object',
+      properties: {
+        location: {
+          type: 'string',
+          description: 'The location to get the weather for.'
+        }
+      },
+      required: ['location']
+    }
   }
 };
 
-export async function getWeather() {
+export async function getWeather({ location }) {
+  const geocodeUrl = new URL('https://api.openweathermap.org/geo/1.0/direct');
+  geocodeUrl.searchParams.append('q', location);
+  geocodeUrl.searchParams.append('limit', '1');
+  geocodeUrl.searchParams.append('appid', OPENWEATHER_API_KEY);
+  const geocodeResponse = await fetch(geocodeUrl);
+  const geocodeData = await geocodeResponse.json();
+  const { lat, lon } = geocodeData[0];
+
   const weatherUrl = new URL('https://api.openweathermap.org/data/2.5/weather');
-  weatherUrl.searchParams.append('lat', 42.3601);
-  weatherUrl.searchParams.append('lon', -71.0589);
+  weatherUrl.searchParams.append('lat', lat);
+  weatherUrl.searchParams.append('lon', lon);
   weatherUrl.searchParams.append('units', 'imperial');
   weatherUrl.searchParams.append('appid', OPENWEATHER_API_KEY);
 
